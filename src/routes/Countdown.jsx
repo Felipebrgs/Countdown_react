6ccb9bdbcb685d9339b5eb40d824b06d80c44e1c
Felipebrgs <felipebrgess@gmail.com>
2024-12-08 17:26:53 -0300

    Enviado

---
+++ src/routes/Countdown.jsx
@@ -0,0 +1,40 @@
+import Title from "../components/Title";
+import Counter from "../components/Counter"
+import { Navigate } from "react-router-dom";
+
+import useCountdown from '../hooks/useCountdown.jsx';
+import { CountdownContext } from "../context/CountdownContext.jsx";
+import { useContext, useEffect } from "react";
+
+
+
+const Countdown = () => {
+
+  const {event} = useContext(CountdownContext);
+
+  if(!event) return <Navigate to="/" replace/>;
+
+  let eventTitle = null;
+
+  if (event.title) eventTitle = event.title;
+
+  let eventColor = null;
+
+  if (event.color) eventColor = event.color;
+
+    const [day, hour, minute, second] = useCountdown(event.date);
+
+    return (
+ <>
+    <Title title={eventTitle} eventColor={eventColor} />
+    <div className="countdown-container">
+      <Counter title="Dias" number={day} eventColor={eventColor}/>
+      <Counter title="Horas" number={hour} eventColor={eventColor}/>
+      <Counter title="Minutos" number={minute} eventColor={eventColor}/>
+      <Counter title="Segundos" number={second} eventColor={eventColor}/>
+    </div>
+ </>
+  )
+}
+
+export default Countdown;
